ce39203ddd10fcfd036e841f8ba5a052265cb014
ageorge95 <arteni.george.daniel@gmail.com>
2025-08-04 10:17:41 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = 'XT Exchange python wrapper'
+LONG_DESCRIPTION = 'Various scripts that can be used to easily interact with the XT exchange.'
+
+setup(
+    name="XT",
+    version=VERSION,
+    author="ageorge95",
+    author_email="arteni.george.daniel@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    extras_require={},
+    keywords=['python', 'pyXT', 'XT']
+)
